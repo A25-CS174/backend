9fca13a6253715ded1b9a543ff7ec6fb84ec475b
rakasatriaefendi <rakasatriaefendi57@gmail.com>
2025-12-12 14:35:57 +0700

    fix error on CORS

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ const app = express();
 
 //middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL.split(","),
-  "http://localhost:5173",
-  credentials: true,
+  origin: [
+    process.env.FRONTEND_URL.split(","),
+    "http://localhost:5173"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 
